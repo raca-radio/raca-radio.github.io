f3834227595830019544af63a5a02637696b279f
eivillamuelas <eivillamuelas@gmail.com>
2021-03-03 15:52:08 +0100

    h

--- js/infoTime.js
+++ js/infoTime.js
@@ -1,68 +1,85 @@
 $(document).ready(function() {
 
     var current = new Date();
-        var hour = current.getHours();
-        if (hour.toString().length<2){
-            hour = "0" + hour.toString();
-        }
-        var minute = current.getMinutes();
-        if (minute.toString().length<2){
-            minute = "0" + minute.toString();
-        }
-        var time = hour + ":" + minute;
-        console.log(time);
+    var hour = current.getHours();
+    if (hour.toString().length<2){
+        hour = "0" + hour.toString();
+    }
+    var minute = current.getMinutes();
+    if (minute.toString().length<2){
+        minute = "0" + minute.toString();
+    }
+    var time = hour + ":" + minute;
+    console.log(time);
 
-        var element_time = $( ".info-time" );
-        for(var i = 0; i < element_time.length; i++)
-        {
-            var inicio_time = element_time[i].getAttribute("inicio");
-            var fin_time = element_time[i].getAttribute("fin");
+    var element_time = $( ".info-time" );
+    for(var i = 0; i < element_time.length; i++)
+    {
+        var inicio_time = element_time[i].getAttribute("inicio");
+        var fin_time = element_time[i].getAttribute("fin");
 
-            if (fin_time<=time) {
-                element_time[i].style.display = "none";
+        if (fin_time<=time) {
+            element_time[i].style.display = "none";
                 
+        } else {
+            if (inicio_time<=time){
+                element_time[i].style.display = "inline-flex";
             } else {
-                if (inicio_time<=time){
-                    element_time[i].style.display = "inline-flex";
-                } else {
-                    element_time[i].style.display = "none";
-                }
+                element_time[i].style.display = "none";
             }
-        
         }
+        
+    }
+    
+
+    if ($('.info-time:visible').length == 0){
+        $("#info-empty").css( "display", "inline-flex" );
+    } else {
+        $("#info-empty").css( "display", "none" );
+    }
     
     setInterval(function(){ 
 
         var current = new Date();
-        var hour = current.getHours();
-        if (hour.toString().length<2){
-            hour = "0" + hour.toString();
-        }
-        var minute = current.getMinutes();
-        if (minute.toString().length<2){
-            minute = "0" + minute.toString();
-        }
-        var time = hour + ":" + minute;
-        console.log(time);
+    var hour = current.getHours();
+    if (hour.toString().length<2){
+        hour = "0" + hour.toString();
+    }
+    var minute = current.getMinutes();
+    if (minute.toString().length<2){
+        minute = "0" + minute.toString();
+    }
+    var time = hour + ":" + minute;
+    console.log(time);
 
-        var element_time = $( ".info-time" );
-        for(var i = 0; i < element_time.length; i++)
-        {
-            var inicio_time = element_time[i].getAttribute("inicio");
-            var fin_time = element_time[i].getAttribute("fin");
+    var empty = true;
 
-            if (fin_time<=time) {
-                element_time[i].style.display = "none";
+    var element_time = $( ".info-time" );
+    for(var i = 0; i < element_time.length; i++)
+    {
+        var inicio_time = element_time[i].getAttribute("inicio");
+        var fin_time = element_time[i].getAttribute("fin");
+
+        if (fin_time<=time) {
+            element_time[i].style.display = "none";
                 
+        } else {
+            if (inicio_time<=time){
+                element_time[i].style.display = "inline-flex";
+                empty = false;
             } else {
-                if (inicio_time<=time){
-                    element_time[i].style.display = "inline-flex";
-                } else {
-                    element_time[i].style.display = "none";
-                }
+                element_time[i].style.display = "none";
             }
-        
         }
+        
+    }
+
+    if ($('.info-time:visible').length == 0){
+        $("#info-empty").css( "display", "inline-flex" );
+    } else {
+        $("#info-empty").css( "display", "none" );
+    }
+
         
     
     }, 60000);
